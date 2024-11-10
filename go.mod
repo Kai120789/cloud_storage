26d7bd30722a3e7b6b44bcf59171f65ec96ed760
Kai120789 <zoomer120789@gmail.com>
2024-11-10 22:34:21 +0300

    feat: add config

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.23.1
 require go.uber.org/zap v1.27.0
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/joho/godotenv v1.5.1
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.29.0
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,64 @@
 package config
+
+import (
+	"flag"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"go.uber.org/zap/zapcore"
+)
+
+var AppConfig *Config
+
+type Config struct {
+	ServerAddress string
+	DBDSN         string
+	LogLevel      string
+	SecretKey     string
+}
+
+func GetConfig() (*Config, error) {
+	_ = godotenv.Load()
+
+	cfg := &Config{}
+
+	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "URL and port to run server")
+	flag.StringVar(&cfg.DBDSN, "d", "", "DBDSN for database")
+
+	cfg.SecretKey = getEnvStringOrDefault("SECRET_KEY", "default")
+	cfg.ServerAddress = getEnvStringOrDefault("SERVER_ADDRESS", "localhost:8080")
+	cfg.DBDSN = getEnvStringOrDefault("DBDSN", "")
+
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		cfg.LogLevel = envLogLevel
+	} else {
+		cfg.LogLevel = zapcore.ErrorLevel.String()
+	}
+
+	flag.Parse()
+
+	AppConfig = cfg
+
+	return cfg, nil
+}
+
+func getEnvStringOrDefault(name, defaultValue string) string {
+	if envString := os.Getenv(name); envString != "" {
+		return envString
+	}
+
+	return defaultValue
+}
+
+func getEnvIntOrDefault(name string, defaultValue int) (*int, error) {
+	if envInt := os.Getenv(name); envInt != "" {
+		intEnvInt, err := strconv.Atoi(envInt)
+		if err != nil {
+			return nil, err
+		}
+		return &intEnvInt, nil
+	}
+
+	return &defaultValue, nil
+}
