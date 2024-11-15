dfb16ddc5a2098080d2813938eca1cb8d5be0ab3
Kai120789 <zoomer120789@gmail.com>
2024-11-15 22:32:36 +0300

    feat: add user funcs to postgres

--- go.mod
+++ go.mod
@@ -13,12 +13,14 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	golang.org/x/text v0.20.0 // indirect
 )
 
 require (
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/joho/godotenv v1.5.1
 	go.uber.org/multierr v1.10.0 // indirect
--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/jackc/pgx"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"go.uber.org/zap"
 )
@@ -61,5 +62,67 @@ func (s *Storage) SearchFiles() error {
 
 // func return items in directory
 func (s *Storage) ListDirectory() error {
+	return nil
+}
+
+// register new user
+func (s *Storage) RegisterNewUser(body dto.User) (*models.UserToken, error) {
+	var id uint
+	query := `INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id`
+	err := s.Conn.QueryRow(context.Background(), query, body.Login, body.Password).Scan(&id)
+	if err != nil {
+		return nil, err
+	}
+
+	userRet, err := s.GetAuthUser(uint(id))
+	if err != nil {
+		return nil, err
+	}
+
+	return userRet, nil
+}
+
+// login user
+func (s *Storage) AuthorizateUser(body dto.User) (*uint, *string, error) {
+	var id uint
+	var passwordHash string
+
+	query := `SELECT id, password FROM users WHERE username=$1`
+	err := s.Conn.QueryRow(context.Background(), query, body.Login).Scan(&id, &passwordHash)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, nil, fmt.Errorf("user not found")
+		}
+		return nil, nil, err
+	}
+
+	return &id, &passwordHash, nil
+}
+
+// get auth user
+func (s *Storage) GetAuthUser(id uint) (*models.UserToken, error) {
+	query := `SELECT * FROM user_token WHERE user_id=$1`
+	row := s.Conn.QueryRow(context.Background(), query, id)
+
+	var token models.UserToken
+	err := row.Scan(&token.ID, &token.UserID, &token.RefreshToken)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &token, nil
+}
+
+// logout user
+func (s *Storage) UserLogout(id uint) error {
+	query := `DELETE FROM user_token WHERE user_id=$1`
+	_, err := s.Conn.Exec(context.Background(), query, id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
